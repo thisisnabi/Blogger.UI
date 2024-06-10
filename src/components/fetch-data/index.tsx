10941ad1c92aabbb77f5f757097ff302a5bc594e
sara <saragholampour971@yahoo.com>
2024-06-10 23:08:00 +0330

    add FetchData

---
+++ src/components/fetch-data/index.tsx
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import React, { useEffect, useMemo, useState } from 'react'
+
+type Props<T> = {
+  children: (data: T | null, error: string | null) => React.ReactNode
+  req: () => Promise<T>
+  dependencies: any[]
+}
+
+function FetchData<T = any>(props: Props<T>) {
+  const { children, dependencies, req } = props
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+  const [response, setResponse] = useState<T>()
+
+  const abortController = useMemo(() => new AbortController(), [dependencies])
+
+  const fetchData = () => {
+    setIsLoading(true)
+    return req()
+      .then((res) => {
+        setResponse(res)
+        setIsLoading(false)
+        return res
+      })
+      .catch((er) => {
+        setError(er.toString)
+
+        if (!axios.isCancel(er)) {
+          setIsLoading(false)
+        }
+      })
+  }
+
+  useEffect(() => {
+    fetchData().then()
+
+    return () => {
+      if (typeof abortController.abort === 'function') abortController.abort()
+    }
+  }, dependencies)
+
+  if (isLoading) return <div>loading</div>
+
+  return <>{children(response, error)}</>
+}
+
+export default FetchData
