9311fb8895299e81b1261e0b75ace347ad9a75ab
sara <saragholampour971@yahoo.com>
2024-06-14 13:10:54 +0330

    fix 404 page style

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -2,8 +2,8 @@ import PNF from 'assets/img/404.png'
 
 const PageNotFound = () => {
   return (
-    <div className={'w-full h-full flex items-center justify-center'}>
-      <img src={PNF} alt={'page-not-found'} />
+    <div className={'w-full h-full'}>
+      <img src={PNF} alt={'page-not-found'} className={'mx-auto'} />
     </div>
   )
 }
