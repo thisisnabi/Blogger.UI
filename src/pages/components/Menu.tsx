59cbeb707ff4ce73b10d72ebd53f8651fe7fb1f2
sara <saragholampour971@yahoo.com>
2024-06-14 13:23:25 +0330

    handle current page style from menu in sidebar

--- src/pages/components/Menu.tsx
+++ src/pages/components/Menu.tsx
@@ -6,49 +6,55 @@ import {
   TagIcon,
 } from '@heroicons/react/outline'
 import { ChevronRightIcon } from '@heroicons/react/solid'
-import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+import { Link, useLocation } from 'react-router-dom'
 
 const menuItems = [
   { label: 'Home', icon: <HomeIcon className={'w-4'} />, to: '/' },
-  { label: 'Tag', icon: <TagIcon className={'w-4'} />, to: '/' },
-  { label: 'Archives', icon: <ArchiveIcon className={'w-4'} />, to: '/' },
+  { label: 'Tag', icon: <TagIcon className={'w-4'} />, to: '/tags' },
+  {
+    label: 'Archives',
+    icon: <ArchiveIcon className={'w-4'} />,
+    to: '/archives',
+  },
   {
     label: 'About',
     icon: <InformationCircleIcon className={'w-4'} />,
-    to: '/',
+    to: '/about',
   },
-  { label: 'subscribe', icon: <CheckIcon className={'w-4'} />, to: '/' },
+  {
+    label: 'subscribe',
+    icon: <CheckIcon className={'w-4'} />,
+    to: '/subscribe',
+  },
 ]
 
 const Menu = () => {
+  const { pathname } = useLocation()
+
   return (
-    <ul>
+    <div>
       {menuItems?.map((node, index) => (
-        <li
+        <Link
           key={`menu-item-{${index}}`}
-          className={
-            'group/item h-[56px] hover:bg-primary hover:text-white rounded-4 px-5'
-          }
+          to={node.to}
+          className={classNames(
+            'flex items-center group/item !h-[56px] hover:text-primary hover:text-white rounded-4 px-5 gap-x-3 w-full h-full text-gray1 group-hover/item:text-white',
+            pathname === node.to ? 'bg-primary !text-white' : null
+          )}
         >
-          <Link
-            to={node.to}
+          <span>{node.icon}</span>
+          <span className={'text-4'}>{node.label}</span>
+          <span
             className={
-              'flex items-center gap-x-3 w-full h-full text-gray1 group-hover/item:text-white'
+              'invisible group-hover/item:visible text-black ml-auto font-semibold text-primary'
             }
           >
-            <span>{node.icon}</span>
-            <span className={'text-4'}>{node.label}</span>
-            <span
-              className={
-                'invisible group-hover/item:visible text-black ml-auto font-semibold group-hover/item:text-white'
-              }
-            >
-              <ChevronRightIcon className={'w-4'} />
-            </span>
-          </Link>
-        </li>
+            <ChevronRightIcon className={'w-4'} />
+          </span>
+        </Link>
       ))}
-    </ul>
+    </div>
   )
 }
 
