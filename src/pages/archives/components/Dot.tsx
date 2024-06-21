a2618e983d2aa473d7bb61f80a8e96709050ee68
sara <saragholampour971@yahoo.com>
2024-06-21 17:02:20 +0330

    fix dot shrink

--- src/pages/archives/components/Dot.tsx
+++ src/pages/archives/components/Dot.tsx
@@ -1,6 +1,10 @@
 const Dot = () => {
   return (
-    <div className={'w-4 h-4 lg:w-5 lg:h-5 bg-primary rounded-full p-[6px]'}>
+    <div
+      className={
+        'w-4 h-4 lg:w-5 lg:h-5 bg-primary rounded-full p-[6px] shrink-0'
+      }
+    >
       <div
         style={{ backgroundColor: 'InfoBackground' }}
         className={'rounded-full w-full h-full'}
