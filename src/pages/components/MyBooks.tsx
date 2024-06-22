a23eb98d60df266fb32f81bb97f3f03e375f1f81
Nabi Karampour <N.Karampour@Alibaba.ir>
2024-06-22 09:04:35 +0330

    Change titles

--- src/pages/components/MyBooks.tsx
+++ src/pages/components/MyBooks.tsx
@@ -6,13 +6,13 @@ const MyBooks = () => {
     <div className={'w-full bg-white rounded-3 pt-4 shadow-md'}>
       <div className={'flex items-center gap-x-2 font-medium mb-[10px] px-6'}>
         <ClipboardListIcon className={'text-primary w-5'} />
-        <span className={'text-4'}>My Books</span>
+        <span className={'text-4'}>My Calender</span>
       </div>
 
       <a href={'https://lu.ma/thisisnabi'} target={'_blank'} rel="noreferrer">
         <img
           src={books}
-          alt={'my-book'}
+          alt={'my-calender'}
           className={'w-full h-[130px] rounded-2 rounded-t-none object-cover '}
         />
       </a>
